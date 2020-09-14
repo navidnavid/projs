e45315743e7dad461f593acbadc4a832e9f0596b
Navid Otadi <navid.otadi@gmail.com>
2020-09-14 19:51:33 +0200

    first commit

---
+++ Chord/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chord.Models;
+
+namespace Chord.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName)
+        {
+            ViewBag.Name = string.Format("Name: {0} {1}", firstName, lastName);
+            return View();
+        }
+
+
+
+
+
+    }
+}
